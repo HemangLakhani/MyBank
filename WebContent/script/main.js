6c304c1c400da87c775f2d758c311a9f6dcf0712
Hemang Lakhani <hemanglakhani10@gmail.com>
2018-06-15 08:14:27 -0400

    Add files via upload

--- WebContent/script/main.js
+++ WebContent/script/main.js
@@ -1,1 +1,21 @@
+// Create the dropdown base
+$("<select />").appendTo("nav");
 
+// Create default option "Go to..."
+$("<option />", {
+   "selected": "selected",
+   "value"   : "",
+   "text"    : "Go to..."
+}).appendTo("nav select");
+
+// Populate dropdown with menu items
+$("nav a").each(function() {
+ var el = $(this);
+ $("<option />", {
+     "value"   : el.attr("href"),
+     "text"    : el.text()
+ }).appendTo("nav select");
+});
+$("nav select").change(function() {
+	  document.submit();
+	});
